4ccc1029aeb54b8eaba609f8f058d46be2598796
dotDone <liam.simpson.91@gmail.com>
2022-02-11 08:54:27 +0800

    Basic list view no tutorial

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,148 +8,102 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('M Y   A P P'),
-      ),
-      body: ListView(
-        children: [
-          Image.asset(
-            './lib/assets/images/lake.jpg',
-            width: 600,
-            height: 260,
-            fit: BoxFit.cover,
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              (Icons.share),
+              size: 20,
+            ),
           ),
-          titleSection,
-          buttonSection,
-          textSection,
         ],
       ),
-    );
-  }
-}
-
-class FavouriteWidget extends StatefulWidget {
-  const FavouriteWidget({Key? key}) : super(key: key);
-
-  @override
-  _FavouriteWidgetState createState() => _FavouriteWidgetState();
-}
-
-class _FavouriteWidgetState extends State<FavouriteWidget> {
-  bool _isFavourited = true;
-  int _favouriteCount = 41;
-
-  void _toggleFavourite() {
-    setState(() {
-      if (_isFavourited) {
-        _favouriteCount -= 1;
-        _isFavourited = false;
-      } else {
-        _favouriteCount += 1;
-        _isFavourited = true;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          padding: const EdgeInsets.all(0),
-          child: IconButton(
-            padding: const EdgeInsets.all(0),
-            alignment: Alignment.centerRight,
-            icon: (_isFavourited
-                ? const Icon(Icons.star)
-                : const Icon(Icons.star_border)),
-            color: Colors.red[500],
-            onPressed: _toggleFavourite,
+      body: ListView.builder(
+        itemBuilder: (_, i) => Padding(
+          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 4.0),
+          child: ListTile(
+            leading: Icon(
+              Icons.question_answer,
+              color: Colors.red,
+              size: 30,
+            ),
+            title: Text(
+              'Item $i',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black26,
+                  fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+              'Subtitle $i',
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.grey,
+                  fontStyle: FontStyle.italic),
+            ),
+            trailing: Icon(
+              Icons.bento_rounded,
+              size: 16,
+              color: Colors.deepOrange,
+            ),
+            onTap: () {},
           ),
         ),
-        SizedBox(
-          width: 18,
-          child: SizedBox(
-            child: Text('$_favouriteCount'),
-          ),
-        )
-      ],
-    );
-  }
-}
-
-Widget titleSection = Container(
-  padding: const EdgeInsets.all(32),
-  child: Row(
-    children: [
-      Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        addAutomaticKeepAlives: false,
+        cacheExtent: 100,
+        itemCount: 40,
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey,
+        elevation: 2,
+        child: ListView(
           children: [
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: const Text(
-                'Top bar - in a container',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+            DrawerHeader(
+              child: ListView(children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {},
+                  iconSize: 40,
                 ),
-              ),
+              ]),
             ),
-            const Text(
-              'Bottom bar - text widget',
-              style: TextStyle(
-                color: Colors.grey,
+            ListTile(
+              leading: Icon(
+                Icons.book,
+                size: 20,
+                color: Colors.blue[200],
               ),
+              title: const Text('Page 1'),
+              subtitle: const Text('It\'s the first page'),
+              trailing: Icon(
+                Icons.arrow_right,
+                size: 20,
+                color: Colors.grey[400],
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.book,
+                size: 20,
+                color: Colors.blue[200],
+              ),
+              title: const Text('Page 2'),
+              subtitle: const Text('It\'s the second page'),
+              trailing: Icon(
+                Icons.arrow_right,
+                size: 20,
+                color: Colors.grey[400],
+              ),
+              onTap: () {},
             ),
           ],
         ),
       ),
-      FavouriteWidget(),
-    ],
-  ),
-);
-
-Column _buildButtonColumn(Color color, IconData icon, String label) {
-  return Column(
-    mainAxisSize: MainAxisSize.min,
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Icon(
-        icon,
-        color: color,
-      ),
-      Container(
-        margin: const EdgeInsets.only(top: 8),
-        child: Text(
-          label,
-          style: TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.w400,
-            color: color,
-          ),
-        ),
-      ),
-    ],
-  );
+    );
+  }
 }
-
-Widget buttonSection = Row(
-  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  children: [
-    _buildButtonColumn(Colors.red, Icons.call, 'CALL'),
-    _buildButtonColumn(Colors.red, Icons.near_me, 'ROUTE'),
-    _buildButtonColumn(Colors.red, Icons.share, 'SHARE'),
-  ],
-);
-
-Widget textSection = const Padding(
-  padding: EdgeInsets.all(32),
-  child: Text(
-    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-    'Alps. Situated 1,578 meters above sea level, it is one of the '
-    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-    'half-hour walk through pastures and pine forest, leads you to the '
-    'lake, which warms to 20 degrees Celsius in the summer. Activities '
-    'enjoyed here include rowing, and riding the summer toboggan run.',
-    softWrap: true,
-  ),
-);
