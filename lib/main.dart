d97c9687330328513f029445f55ce86d805fea4d
dotDone <liam.simpson.91@gmail.com>
2022-02-13 14:56:57 +0800

    Added ThemeData objects and main listener

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:myfirstapp/homepage.dart';
+import 'package:myfirstapp/themes.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    currentTheme.addListener(() {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My First App',
+      theme: CustomTheme.lightTheme,
+      darkTheme: CustomTheme.darkTheme,
+      themeMode: currentTheme.currentTheme,
       home: HomePage(),
     );
   }
---
+++ lib/themes.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+CustomTheme currentTheme = CustomTheme();
+
+class CustomTheme with ChangeNotifier {
+  static bool _isDarkTheme = true;
+  ThemeMode get currentTheme => _isDarkTheme ? ThemeMode.dark : ThemeMode.light;
+
+  void toggleTheme() {
+    _isDarkTheme = !_isDarkTheme;
+    notifyListeners();
+  }
+
+  static ThemeData get lightTheme {
+    return ThemeData();
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData();
+  }
+}
