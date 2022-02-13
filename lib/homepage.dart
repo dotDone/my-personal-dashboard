aa6f728bf8472a99160f0372cbbee13c50da3082
dotDone <liam.simpson.91@gmail.com>
2022-02-13 14:41:06 +0800

    cleared tutorial content

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -19,41 +19,6 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemBuilder: (_, i) => Padding(
-          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 4.0),
-          child: ListTile(
-            leading: Icon(
-              Icons.question_answer,
-              color: Colors.red,
-              size: 30,
-            ),
-            title: Text(
-              'Item $i',
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black26,
-                  fontWeight: FontWeight.bold),
-            ),
-            subtitle: Text(
-              'Subtitle $i',
-              style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.grey,
-                  fontStyle: FontStyle.italic),
-            ),
-            trailing: Icon(
-              Icons.bento_rounded,
-              size: 16,
-              color: Colors.deepOrange,
-            ),
-            onTap: () {},
-          ),
-        ),
-        addAutomaticKeepAlives: false,
-        cacheExtent: 100,
-        itemCount: 40,
-      ),
       drawer: Drawer(
         backgroundColor: Colors.grey,
         elevation: 2,
@@ -103,6 +68,10 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
+      ),
+      body: Container(
+        color: Colors.deepPurple[200],
+        child: Placeholder(),
       ),
     );
   }
